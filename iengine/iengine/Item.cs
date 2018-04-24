1a0937b1aacf3da28797ca3183f819b97a70badd
Jack <jackster99@gmail.com>
2018-04-24 15:24:00 +1000

    Initial Project Creation with start of loading from text file

---
+++ iengine/iengine/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iengine
+{
+    class Item
+    {
+
+        public string name;
+        public bool valid;
+        public List<Relation> relations;
+    }
+
+    class Relation
+    {
+        public string name;
+        public string clause;
+    }
+    
+}
---
+++ iengine/iengine/ReadTextFile.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace iengine
+{
+    class ReadTextFile
+    {
+        public List<Item> ReadFile(string fileName)
+        {
+            List<Item> result = new List<Item>();
+            string[] lines = System.IO.File.ReadAllLines(Directory.GetCurrentDirectory() + "/" + fileName + ".txt");
+            bool tell = false;
+            bool ask = false;
+            string[] rules;
+            string[] identifiers = { "=>", "&"};
+            foreach(string s in lines)
+            {
+                if (s == "TELL")
+                {
+                    tell = true;
+                    ask = false;
+
+                }
+                else if (s == "ASK")
+                {
+                    ask = true;
+                    tell = false;
+
+                }
+                else
+                {
+                    if (tell == true)
+                    {
+                        rules = s.Split(';');
+                        foreach (string r in rules)
+                        {
+                            string[] item;
+                            string identifier = string.Join("|", identifiers.ToArray());
+                            //splits the string with the corresponding identifiers and removes any spaces from the split string
+                            item = Regex.Split(r.Replace(" ", string.Empty), @"(" + identifier +@")");
+                            foreach(string i in item)
+                            {
+                                //if item is a identifier
+                                if (identifiers.Any(i.Contains)){
+
+                                }
+                                else
+                                {
+                                    bool newItem = true;
+                                    foreach(Item j in result)
+                                    {
+                                        if (j.name == i)
+                                        {
+                                            newItem = false;
+                                        }
+                                    }
+                                    if(newItem == true)
+                                    {
+                                        result.Add(new Item());
+                                        result.Last().name = i;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else if (ask == true)
+                    {
+
+                    }
+                  
+                }
+            }
+            return result;
+        }
+    }
+}
