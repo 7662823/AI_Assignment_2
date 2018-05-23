82d27185d9d5aa85712571a75896f229c5fa6afc
Jack <jackster99@gmail.com>
2018-05-23 16:44:55 +1000

    Fixed Local Test on visual studio & created TruthTable Test CS file

---
+++ iengine/iengineTests/TruthTableTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace iengine.Tests
+{
+    [TestClass]
+    public class TruthTableTests
+    {
+        [TestMethod]
+        public void TestOutputForGivenExample1()
+        {
+        }
+        [TestMethod]
+        public void TestOutputForGivenExample2()
+        {
+        }
+        [TestMethod]
+        public void TestOutputValidQueryInvalidRules()
+        {
+
+        }
+        [TestMethod]
+        public void TestOutputValidQueryValidRules()
+        {
+
+        }
+        [TestMethod]
+        public void TestOutputInValidQueryValidRules()
+        {
+
+        }
+        [TestMethod]
+        public void TestOutputInValidQueryInValidRules()
+        {
+
+        }
+    }
+}
